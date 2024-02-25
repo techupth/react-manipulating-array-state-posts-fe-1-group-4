0d9535559d734593715c43d196a2a419024b961d
tum <hongsachm66.gmail.com>
2024-02-25 23:47:47 +0700

    tum's work

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -8,6 +8,7 @@ const initialPosts = [
       "Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum.",
     likes: 61,
   },
+
   {
     id: 2,
     title: "Born to Kill",
@@ -15,6 +16,7 @@ const initialPosts = [
       "Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla.",
     likes: 46,
   },
+
   {
     id: 3,
     title: "Ten Shrews, The (Kymmenen riivinrautaa)",
@@ -22,12 +24,14 @@ const initialPosts = [
       "Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis. Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.",
     likes: 50,
   },
+
   {
     id: 4,
     title: "Ciel est Ã  vous, Le (Woman Who Dared, The)",
     content: "Maecenas ut massa quis augue luctus tincidunt.",
     likes: 3,
   },
+
   {
     id: 5,
     title: "Tigger Movie, The",
@@ -41,6 +45,7 @@ const initialPosts = [
       "Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus. Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
     likes: 70,
   },
+
   {
     id: 7,
     title: "Brooklyn Castle",
@@ -48,6 +53,7 @@ const initialPosts = [
       "In hac habitasse platea dictumst. Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh.",
     likes: 46,
   },
+
   {
     id: 8,
     title: "Snow Angels",
@@ -55,12 +61,14 @@ const initialPosts = [
       "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat.",
     likes: 1,
   },
+
   {
     id: 9,
     title: "Rose Tattoo, The",
     content: "Curabitur in libero ut massa volutpat convallis.",
     likes: 20,
   },
+
   {
     id: 10,
     title: "It's A Wonderful World",
@@ -69,7 +77,6 @@ const initialPosts = [
     likes: 6,
   },
 ];
-
 
 function Posts() {
   const [posts, setPosts] = useState(initialPosts);
